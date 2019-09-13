4fb59192ce17655ca8a453cbc5430ee48fe35fd4
katesm <michaelkates@Michaels-MacBook-Pro.local>
2019-09-13 11:57:57 -0400

    init

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # DevTest
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
+# Getting Started
+
+1. Please install NPM if you do not have it already on you machine. 
+https://www.npmjs.com/get-npm
+
+2. Use NPM to install angular cli :
+``` npm install -g @angular/cli ```
+
+3. We recommend using Visual Studio Code :
+https://code.visualstudio.com/
+
+4. We also recommend installing the Angular Language Service extension for Visual Studio Code : https://marketplace.visualstudio.com/items?itemName=Angular.ng-template
+
+
+# Notes
 
 ## Development server
 
@@ -14,14 +28,10 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormField } from '@angular/material/form-field';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSortModule, MatInputModule, MatDialog, MatDialogModule, MatTabsModule, MatExpansionModule, MatDividerModule, MatStepperModule, MatRadioModule, MatTreeModule, MatBottomSheet, MatBottomSheetModule, MatProgressBarModule, MatSelectModule, MatTableModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatFormField,
+    MatDialog,
+    MatSortModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatStepperModule,
+    MatTreeModule,
+    MatBottomSheetModule,
+    MatProgressBarModule,
+    FormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule,
+    MatTableModule,
+    MatBottomSheetModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
