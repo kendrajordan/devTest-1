05d5070ba6eaad32f9c706eb5f23b7c4f832bdaa
Kendra Jordan <kendra.jordan8@gmail.com>
2019-09-21 18:18:33 -0400

    finished post feature and added some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, } from '@angular/material/button';
@@ -9,23 +9,16 @@ import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatSortModule, MatInputModule, MatDialog, MatDialogModule, MatTabsModule, MatExpansionModule, MatDividerModule, MatStepperModule, MatRadioModule, MatTreeModule, MatBottomSheet, MatBottomSheetModule, MatProgressBarModule, MatSelectModule, MatTableModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
-import { PostComponent } from './post/post.component';
-import { GetComponent } from './get/get.component';
+
 import { HttpClientModule} from '@angular/common/http';
 import { BudgetPostService } from 'src/app/budget-post.service';
-import { GetYearComponent } from './get-year/get-year.component';
-import { GetTitleComponent } from './get-title/get-title.component';
-import { GetIdComponent } from './get-id/get-id.component';
+
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    GetComponent,
-    GetYearComponent,
-    GetTitleComponent,
-    GetIdComponent,
+    routingComponents,
 
   ],
   imports: [
@@ -53,8 +46,8 @@ import { NgxPaginationModule } from 'ngx-pagination';
     MatTableModule,
     MatBottomSheetModule,
     HttpClientModule,
-    NgxPaginationModule
-
+    NgxPaginationModule,
+   
   ],
   providers: [BudgetPostService],
   bootstrap: [AppComponent]
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{ Budget } from 'src/app/budget';
 import { BudgetPostService } from 'src/app/budget-post.service';
+import { from } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -9,65 +11,39 @@ import { BudgetPostService } from 'src/app/budget-post.service';
 export class PostComponent implements OnInit {
   budgetModel = new Budget( null, null, "", "");
   budgetsList:Budget[];
-  ifSuccess = false;
   latest = new Budget( null, null, "", "");
   msg = "";
+  result = "";
+  
   constructor(private _budgetPostService: BudgetPostService){ }
   
   ngOnInit() {
-//this.getAllBudget();
-this.getNewId();
-  }
- //Api call for  Budgets
-getAllBudget(){
-  this._budgetPostService.getAllBudgetId()
-  .subscribe(
-    data =>{
-     this.budgetsList = data.data;
-   
-    } 
-  )
-}
-getNewId(){
-  this._budgetPostService.getAllBudgetId()
-  .subscribe(
-    data =>{
-    var data = data.data;
-    var currentId = data[data.length - 1].budgetCodeId;
-    var newId = currentId + 1;
-    this.budgetModel.budgetCodeId = newId;
-    
-    } 
-  )
-}
-onClick(){
-  this.latest = this.budgetModel;
-  this.ifSuccess = true;
-  this.msg = "It works!";
 
-  this.getNewId();
-    this.budgetModel.budgetCode = "";
-    this.budgetModel.fiscalYear = null;
-    this.budgetModel.budgetTitle = "";
-}
+  }
+
+
+
+/*onClick(){
+  this.latest = this.budgetModel;
+  this.msg = "It works!";
+  this.budgetModel = new Budget( null, null, "", "");
+}*/
   onSubmit(){
     this._budgetPostService.addBudget(this.budgetModel)
     .subscribe(
       data => {
-        this.ifSuccess = true;
+        this.msg = data.message;
         var data = data.data;
         this.latest = data;
-        this.msg = "<b>"+data.message+"!</b>";
+        
       },
       error => {
-        this.msg = "<b>"+error.message+"!</b>";
+        this.msg = error;
       }
         
     )
-    this.getNewId();
-    this.budgetModel.budgetCode = "";
-    this.budgetModel.fiscalYear = null;
-    this.budgetModel.budgetTitle = "";
+   
+    this.budgetModel = new Budget( null, null, "", "");
   }
  
 }
